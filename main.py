b6f2423be1bca62cdae6cfc3515f1210174bf630
feasycom <15386139514@163.com>
2026-09-07 14:22:29 +0800

    从零搭建selenium自动打开浏览器并运行的流程,并保存到git仓库中

---
+++ main.py
@@ -0,0 +1,23 @@
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+import time
+
+
+def test_baidu_search():
+    # 初始化 Chrome 浏览器驱动
+    driver = webdriver.Chrome()
+
+    # 打开百度首页
+    driver.get('http://www.baidu.com')
+
+    # 在搜索框输入关键词
+    driver.find_element(By.ID, 'kw').send_keys('selenium')
+
+    # 点击搜索按钮
+    driver.find_element(By.ID, 'su').click()
+
+    # 等待页面加载
+    time.sleep(2)
+
+    # 关闭浏览器
+    driver.quit()
