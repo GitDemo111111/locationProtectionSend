f5cd7a8100d9cad1766ad37a38c589545904840d
feasycom <15386139514@163.com>
2026-09-08 11:06:03 +0800

    重新推送

--- tests/test_baidu.py
+++ tests/test_baidu.py
@@ -4,4 +4,4 @@ from ..main import test_web_form
 def test_baidu_search():
     # 调用main.py中的test_web_form方法
     # 可以传入自定义文本作为参数，例如'Hello Baidu'
-    test_web_form('Hello CCI')
+    test_web_form('Hello CI')
